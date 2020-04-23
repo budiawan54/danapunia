4337023845223521c74444c021671271411eefab
budiawan54 <madebudiawan2@gmail.com>
2020-04-23 15:46:57 +0800

    menambah isi pada model absensi siswa

--- konfigurasi/app/Absensi.php
+++ konfigurasi/app/Absensi.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Absensi extends Model
 {
     //
+    protected $table = 'absensi_siswa';
+    protected $fillable = ['id_siswa','tanggal','ket_absensi'];
+
 }
